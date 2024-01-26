1659718ebf1ac5fa8914d98dbc204d3d416e1160
kbaduwal <khakendra1999@gmail.com>
2024-01-26 22:33:45 +0530

    AOP-@Before Advice

--- 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,5 +1,6 @@
 package com.luv2code.aopdemo;
 
+import com.luv2code.aopdemo.dao.AccountDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +14,21 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO){
 		return runner ->{
-			System.out.println("Hello World");
+			demoTheBeforeAdvice(theAccountDAO);
 		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+		// call the business method
+		theAccountDAO.addAccount();
+
+		// do it again
+		System.out.println("\nlet's call it again! \n");
+
+		// call the business method again
+		theAccountDAO.addAccount();
 	}
 
 }
