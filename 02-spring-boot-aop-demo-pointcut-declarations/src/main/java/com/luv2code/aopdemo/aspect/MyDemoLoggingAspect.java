917ff240903b00f1449ce3b6f881b4a074ad4a46
kbaduwal <khakendra1999@gmail.com>
2024-02-01 23:54:38 +0530

    AOP-@AfterReturning Advice

--- 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,39 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    // add a new advice for @AfterReturning on the find Accounts method
+
+    @AfterReturning(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result")
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result){
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing the @AfterReturning on method: "+method);
+
+        // print out the result of the method call
+        System.out.println("\n=====>>> result is: "+result);
+
+    }
+
+
+
+
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint theJointPoint){
         System.out.println("\n======> Executing @Before advice on method");
--- 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -2,7 +2,13 @@ package com.luv2code.aopdemo.dao;
 
 import com.luv2code.aopdemo.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
+
+    // add a new method: findAccounts()
+
+    List<Account> findAccount();
     void addAccount(Account theAccount, boolean vipFlap);
 
     boolean doWork();
