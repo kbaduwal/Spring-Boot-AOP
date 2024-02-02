044524947473180fb3ca588a81d1d5be83b6c3aa
kbaduwal <khakendra1999@gmail.com>
2024-02-02 23:00:42 +0530

    @AfterReturning Advice-Modify Return value

--- 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -30,9 +30,27 @@ public class MyDemoLoggingAspect {
         // print out the result of the method call
         System.out.println("\n=====>>> result is: "+result);
 
+        // let's post-process the data ... let's modify it :-)
+
+        // convert the account name to uppercase
+        convertAccountNameToUppercase(result);
+
+
     }
 
+    private void convertAccountNameToUppercase(List<Account> result) {
+        // loop through accounts
+        for(Account tempAccount: result) {
+            //get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
 
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+            System.out.println("\n=====>>> result is: "+result);
+
+        }
+
+    }
 
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
